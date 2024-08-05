ab069414b336614c4b2db30d0916f939e422d40c
dhanushs3366 <dhanushs3366@gmail.com>
2024-08-05 15:37:17 +0530

    some cleanup

--- models/LogActivity.go
+++ models/LogActivity.go
@@ -1,11 +1,5 @@
 package models
 
-import (
-	_ "github.com/lib/pq"
-)
-
-// uni placements are happening i need to learn sql so im raw dogging this without any orms
-
 type LoggedActivity struct {
 	Key          int `json:"all_keys"`
 	MiddleClicks int `json:"middle_clicks"`
--- services/logService.go
+++ services/logService.go
@@ -6,7 +6,11 @@ import (
 	"errors"
 	"log"
 	"time"
+
+	_ "github.com/lib/pq"
 )
+
+// uni placements are happening i need to learn sql so im raw dogging this without any orms
 
 // create a new table entry once in a while like every 1 hour update the latest record for the remaining time
 
